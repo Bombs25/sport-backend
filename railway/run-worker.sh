29b49a4fa8dd8df3a56cd429a7439e650afabf3b
IWALINK <iwatrouve@gmail.com>
2026-08-14 19:23:01 +0200

    working on worker

--- railway/run-worker.sh
+++ railway/run-worker.sh
@@ -1,6 +1,6 @@
-# #!/bin/bash
-# # Make sure this file has executable permissions, run `chmod +x railway/run-worker.sh`
+#!/bin/bash
+# Make sure this file has executable permissions, run `chmod +x railway/run-worker.sh`
 
-# # This command runs the queue worker.
-# # An alternative is to use the php artisan queue:listen command
-# php artisan queue:work
+# This command runs the queue worker.
+# An alternative is to use the php artisan queue:listen command
+php artisan queue:work
