dad8294922b875e4a5f00d7955d2558be3a725b4
IWALINK <iwatrouve@gmail.com>
2026-08-17 16:58:52 +0200

    fixin files upload

--- app/Listeners/ImageProcessingListener.php
+++ app/Listeners/ImageProcessingListener.php
@@ -39,7 +39,8 @@ class ImageProcessingListener
     public function handle(ImageProcessingEvent $event): void
     {
         $files = ImageProcessingStoreRequest::validatedFileList($event->files);
-
+        Log::info('all is good from here');
+        return ;
         /** @var FilesystemAdapter $disk staging privé ({@see ImageProcessingInterface::STAGING_DISK}) */
         $disk = Storage::disk(ImageProcessingInterface::STAGING_DISK);
         $batchKey = $event->uniqueKey;
