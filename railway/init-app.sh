2f9807b50b0c60548e7b9b8be12ecbce49254009
IWALINK <iwatrouve@gmail.com>
2026-08-18 12:45:27 +0200

    added the init app file

--- railway/init-app.sh
+++ railway/init-app.sh
@@ -1,18 +1,18 @@
-# #!/bin/bash
-# # Make sure this file has executable permissions, run `chmod +x railway/init-app.sh`
+#!/bin/bash
+# Make sure this file has executable permissions, run `chmod +x railway/init-app.sh`
 
-# # Exit the script if any command fails
-# set -e
+# Exit the script if any command fails
+set -e
 
-# # Run migrations
-# #php artisan migrate:fresh --force
+# Run migrations
+php artisan migrate --force
 
-# # Clear cache--
-# php artisan optimize:clear
+# Clear cache--
+php artisan optimize:clear
 
-# # Cache the various components of the Laravel application
-# php artisan config:cache
-# php artisan event:cache
-# php artisan route:cache
-# php artisan view:cache
+# Cache the various components of the Laravel application
+php artisan config:cache
+php artisan event:cache
+php artisan route:cache
+php artisan view:cache
 
