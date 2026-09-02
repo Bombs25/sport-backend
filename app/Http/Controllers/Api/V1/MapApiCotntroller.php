819d0348687e961b23f28e786fe543f5f143d0b4
IWALINK <iwatrouve@gmail.com>
2026-09-02 22:27:22 +0200

    added region column

--- app/Http/Controllers/Api/V1/MapApiCotntroller.php
+++ app/Http/Controllers/Api/V1/MapApiCotntroller.php
@@ -48,8 +48,7 @@ class MapApiCotntroller extends Controller
 
     public function add_region()
     {
-        DB::table('teams')->where('region', "=", null)
-            ->orWhere('region', "=", "")
+        DB::table('teams')
             ->chunkById(100, function ($teams) {
                 foreach ($teams as $team) {
                     DB::table('teams')
@@ -59,3 +58,6 @@ class MapApiCotntroller extends Controller
             });
     }
 }
+
+// ->where('region', "=", null)
+//             ->orWhere('region', "=", "")
